d46cae6447261568112f35df134b96c0572ed7dc
saggins <win32prog@gmail.com>
2020-05-16 22:29:31 +0000

    discordbot

---
+++ src/run.php
@@ -0,0 +1,17 @@
+<?php
+
+include __DIR__.'/../vendor/autoload.php';
+$discord = new \Discord\Discord([
+    'token' => getenv("discordToken", true),
+]);
+
+$discord->on('ready', function ($discord) {
+    echo "Bot is ready.", PHP_EOL;
+    // Listen for events here
+    $discord->on('message', function ($message) {
+        echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
+    });
+});
+
+$discord->run();
+?>
