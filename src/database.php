bb769d65a6164284721b7b961850f7bb42b256cd
saggins <saggins@localhost.localdomain>
2020-06-26 19:08:17 -0700

    Reset password and removed password

--- src/database.php
+++ src/database.php
@@ -5,9 +5,9 @@ use xPaw\MinecraftPing;
 use xPaw\MinecraftPingException;
 
 function vault() {
-    $vaultDSN = "mysql:dbname=s7015_sagg-vault;host=na.sql.titannodes.com:3306";#getenv("vaultDSN", true);
-    $vaultUser = "u7015_wRgQaDZSwH";#getenv("vaultUser",true);
-    $vaultPass = ".YIIKA^sBGrwBYdM9Zz6.Ins";#getenv("vaultPass", true);
+    $vaultDSN = getenv("vaultDSN", true);
+    $vaultUser = getenv("vaultUser",true);
+    $vaultPass = getenv("vaultPass", true);
     $pdo = new PDO($vaultDSN, $vaultUser, $vaultPass);
     $statement = $pdo->query("SELECT player_uuid, player_name, money FROM eco_accounts ORDER BY `eco_accounts`.`money` DESC");
     $row = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -15,9 +15,9 @@ function vault() {
 }
 
 function blocks(){
-	$blocksDSN = "mysql:dbname=s7015_sagginmc;host=na.sql.titannodes.com:3306";#getenv("blocksDSN", true);
-    $blocksUser = "u7015_MniKi0yIZh";#getenv("blocksUser",true);
-    $blocksPass = "O2^ZeE^DAr0M!wp0R9a6lcbM";#getenv("blocksPass", true);
+	$blocksDSN = getenv("blocksDSN", true);
+    $blocksUser = getenv("blocksUser",true);
+    $blocksPass = getenv("blocksPass", true);
     $pdo = new PDO($blocksDSN, $blocksUser, $blocksPass);
     $statement = $pdo->query("SELECT player_uuid, player_name, money FROM eco_accounts ORDER BY `eco_accounts`.`money` DESC");
     $row = $statement->fetchAll(PDO::FETCH_ASSOC);
